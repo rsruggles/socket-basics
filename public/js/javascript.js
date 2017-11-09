9be662422f24703930342a31f2dbeef48126cd78
Rian Ruggles <rs.ruggles@gmail.com>
2017-11-08 21:52:19 -0600

    Added custom message event

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -24,3 +24,8 @@ var socket = io();
 socket.on('connect', function () {
   console.log('Connected to socket.io server!');
 });
+
+socket.on('message', function (message) {
+  console.log('New Message:');
+  console.log(message.text);
+});
--- server.js
+++ server.js
@@ -8,8 +8,18 @@ app.use(express.static(__dirname + '/public'));
 
 // io connection event. We log a console message when a user joins the connection here
 // there are plenty of other io events in their documentation
-io.on('connection', function () {
+io.on('connection', function (socket) { // socket passed is associated with the individual users socket, but the socket scope isn't required for basic functionality
   console.log('User connect via socket.io!');
+  
+  socket.on('message', function (message) {
+    console.log('Message recieved: ' + message.text);
+    // io.emit sends to everyone
+    socket.broadcast.emit('message', message); // socket.emit sends to everyone except the sender
+  });
+  
+  socket.emit('message', {
+    text: 'Welcome to the chat application!'
+  });
 });
 
 http.listen(PORT, function(){
