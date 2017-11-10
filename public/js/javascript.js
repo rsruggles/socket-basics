9b8a34e3768d36a6673c80c76bc8651950a107ae
Rian Ruggles <rs.ruggles@gmail.com>
2017-11-09 22:00:23 -0600

    Added timestamp to UI messages

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -26,10 +26,11 @@ socket.on('connect', function () {
 });
 
 socket.on('message', function (message) {
+  var momentTimeStamp = moment.utc(message.timestamp);
   console.log('New Message:');
   console.log(message.text);
   
-  jQuery('.messages').append('<p>' + message.text + '</p>');
+  jQuery('.messages').append('<p><strong>' + momentTimeStamp.local().format('h:mm a') + ': </strong>' + message.text + '</p>');
 });
 
 // Handles Submitting New Message
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var moment = require('moment');
+var now = moment();
 
 app.use(express.static(__dirname + '/public'));
 
@@ -15,11 +17,15 @@ io.on('connection', function (socket) { // socket passed is associated with the 
     console.log('Message recieved: ' + message.text);
     // io.emit sends to everyone
     //socket.broadcast.emit('message', message); // socket.emit sends to everyone except the sender
+    message.timestamp = moment().valueOf();
     io.emit('message', message);
   });
   
+  // timestamp property
+  
   socket.emit('message', {
-    text: 'Welcome to the chat application!'
+    text: 'Welcome to the chat application!',
+    timestamp: moment().valueOf()
   });
 });
 
