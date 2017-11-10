9f463fc4338d4829229421200046a107704805e2
Rian Ruggles <rs.ruggles@gmail.com>
2017-11-10 17:31:22 -0600

    Add support for chat lobbies

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -25,9 +25,14 @@ var socket = io();
 
 console.log(name + ' wants to join ' + room);
 
+jQuery('.room-title').text(room);
 
 socket.on('connect', function () {
   console.log('Connected to socket.io server!');
+  socket.emit('joinRoom', {
+    name: name,
+    room: room
+  });
 });
 
 socket.on('message', function (message) {
--- server.js
+++ server.js
@@ -8,17 +8,29 @@ var now = moment();
 
 app.use(express.static(__dirname + '/public'));
 
+var clientInfo = {};
+
 // io connection event. We log a console message when a user joins the connection here
 // there are plenty of other io events in their documentation
 io.on('connection', function (socket) { // socket passed is associated with the individual users socket, but the socket scope isn't required for basic functionality
   console.log('User connect via socket.io!');
+  
+  socket.on('joinRoom', function (req) {
+    clientInfo[socket.id] = req;
+    socket.join(req.room);
+    socket.broadcast.to(req.room).emit('message', {
+      name: 'System',
+      text: req.name + ' has joined!',
+      timestamp: moment().valueOf
+    });
+  });
   
   socket.on('message', function (message) {
     console.log('Message recieved: ' + message.text);
     // io.emit sends to everyone
     //socket.broadcast.emit('message', message); // socket.emit sends to everyone except the sender
     message.timestamp = moment().valueOf();
-    io.emit('message', message);
+    io.to(clientInfo[socket.id].room).emit('message', message);
   });
   
   // timestamp property
