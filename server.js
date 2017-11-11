836fee288094bf6a5db05169b727c819a111be5c
Rian Ruggles <rs.ruggles@gmail.com>
2017-11-10 18:29:20 -0600

    Added disconnect event and message

--- server.js
+++ server.js
@@ -15,6 +15,19 @@ var clientInfo = {};
 io.on('connection', function (socket) { // socket passed is associated with the individual users socket, but the socket scope isn't required for basic functionality
   console.log('User connect via socket.io!');
   
+  socket.on('disconnect', function () {
+    var userData = clientInfo[socket.id];
+    if (typeof userData !== 'undefined') {
+      socket.leave(userData.room);
+      io.to(userData.room).emit('message', {
+        name: 'System',
+        text: userData.name + ' has left the lobby.',
+        timestamp: moment().valueOf()
+      });
+      delete clientInfo[socket.id];
+    }
+  });
+  
   socket.on('joinRoom', function (req) {
     clientInfo[socket.id] = req;
     socket.join(req.room);
